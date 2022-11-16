39ac82dfa9cd0e0f5f7e0a7ca45decf517fca1fc
Vincent <vkl2109@columbia.edu>
2022-11-16 09:53:51 -0500

    Commented out bad code

--- index.js
+++ index.js
@@ -6,6 +6,51 @@ let globalUserID = '';
 let playlistDescription = '';
 const redirectURI = 'http://127.0.0.1:5500/index.html';
 
+// toggle About button
+const about = document.getElementById('about');
+const footer = document.getElementById('footer');
+about.addEventListener('click', () => {
+    footer.classList.toggle('hidden');
+})
+
+// get name
+const loginForm = document.getElementById('loginForm');
+const loginFormDiv = document.getElementById('loginFormDiv');
+const logoutForm = document.getElementById('logoutForm');
+const loggedIn = document.getElementById('loggedIn');
+const userName = document.getElementById('userID');
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    loginFormDiv.classList.toggle('hidden');
+    logoutForm.classList.toggle('hidden');
+    loggedIn.classList.toggle('hidden');
+    globalUserID = loginForm.loginInput.value;
+    userName.innerText = 'Name: ' + globalUserID;
+    loginForm.reset();
+})
+logoutForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    loginFormDiv.classList.toggle('hidden');
+    logoutForm.classList.toggle('hidden');
+    loggedIn.classList.toggle('hidden');
+    globalUserID = '';
+})
+
+// get user input for sentence
+const input = document.getElementById('input');
+input.addEventListener('submit', (e) => {
+    e.preventDefault();
+    cards.replaceChildren();
+    let sentence = input.sentenceInput.value;
+    playlistDescription = sentence;
+    let words = sentence.split(' ');
+    words.forEach(async (word) => {
+        let newTrack = await testWords(word);
+        console.log(newTrack);
+    });
+    input.reset();
+})
+
 // generate new access token using client ID and client secret
 const getToken = async () => {
     const result = await fetch('https://accounts.spotify.com/api/token', {
@@ -21,106 +66,7 @@ const getToken = async () => {
     return data.access_token;
 }
 
-// toggle About button
-const about = document.getElementById('about');
-const footer = document.getElementById('footer');
-about.addEventListener('click', () => {
-    footer.classList.toggle('hidden');
-})
-
-// get userID
-const loginForm = document.getElementById('loginForm');
-const loginFormDiv = document.getElementById('loginFormDiv');
-const logoutForm = document.getElementById('logoutForm');
-const loggedIn = document.getElementById('loggedIn');
-const userID = document.getElementById('userID');
-loginForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    loginFormDiv.classList.toggle('hidden');
-    logoutForm.classList.toggle('hidden');
-    loggedIn.classList.toggle('hidden');
-    globalUserID = loginForm.loginInput.value;
-    userID.innerText = 'Name: ' + globalUserID;
-    loginForm.reset();
-})
-logoutForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    loginFormDiv.classList.toggle('hidden');
-    logoutForm.classList.toggle('hidden');
-    loggedIn.classList.toggle('hidden');
-    globalUserID = '';
-})
-
-// // create a new playlist logic button
-// const newPlaylist = document.getElementById('newPlaylist');
-// const createPlaylist = document.getElementById('createPlaylist');
-// createPlaylist.addEventListener('click', () => {
-//     if (globalUserID == '') {
-//         newPlaylist.innerText = 'Must Log In First!!';
-//     }
-//     else if (playlistDescription == '') {
-//         newPlaylist.innerText = 'Must Insert Valid Sentence!!';
-//     }
-//     else {
-
-//     }
-// })
-
-// generate authorization code token
-const getAuthToken = async () => {
-    const result = await fetch ('https://accounts.spotify.com/authorize?client_id=' 
-    + clientId + '&scopes=playlist-read-public&response_type=code&redirect+uri=' + encodeURIComponent(redirectURI))
-    const data = result.json();
-    return data;
-}
-
-// generate new playlist using userID
-const generatePlaylist = async (userID) => {
-    let accessToken = await getToken();
-    const result = await fetch(`https://api.spotify.com/v1/users/${userID}/playlists`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + accessToken
-        },
-        body: JSON.stringify({
-            "name": "Newly Generated Playlist!",
-            "description": "testing",
-            "public": true
-        })
-    });
-    const newPlaylist = await result.json();
-    return newPlaylist;
-}
-
-
-// get artist ID from search result
-const getArtist = async (keyword, token) => {
-    const result = await fetch ('https://api.spotify.com/v1/search?q=' + keyword + '&type=artist', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + token
-        }
-    })
-    const data = result.json();
-    return data;
-}
-
-// get user ID
-const getUser = async (token) => {
-    const result = await fetch ('https://api.spotify.com/v1/me', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + token
-        }
-    })
-    const data = result.json();
-    return data;
-}
-
-// get artist ID from search result
+// get tracks from search result
 const getTracks = async (keyword, token) => {
     const result = await fetch ('https://api.spotify.com/v1/search?q=' + keyword + '&type=track', {
         method: 'GET',
@@ -133,48 +79,6 @@ const getTracks = async (keyword, token) => {
     return data;
 }
 
-// generate top ten tracks of artist
-const getTopTracks = async (artistID, token) => {
-    const result = await fetch (`https://api.spotify.com/v1/artists/${artistID}/top-tracks?country=US`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + token
-        }
-    })
-    const data = result.json();
-    return data;
-}
-
-// get user input for artist name
-const input = document.getElementById('input');
-input.addEventListener('submit', (e) => {
-    e.preventDefault();
-    cards.replaceChildren();
-    let sentence = input.artistName.value;
-    playlistDescription = sentence;
-    let words = sentence.split(' ');
-    words.forEach(async (word) => {
-        let newTrack = await testWords(word);
-        console.log(newTrack);
-    });
-    input.reset();
-})
-
-// create new artist card
-const makeArtistCard = (track) => {
-    const card = document.createElement('div');
-    card.classList.add('card');
-    const body = document.createElement('div');
-    body.classList.add('card-body');
-    const p = document.createElement('p');
-    p.classList.add('card-text');
-    p.innerText = track.name;
-    body.append(p);
-    card.append(body);
-    cards.append(card);
-}
-
 // create new embed playlist
 const makeEmbed = (track) => {
     const card = document.createElement('div');
@@ -185,7 +89,7 @@ const makeEmbed = (track) => {
     upperDiv.setAttribute('id', 'cardUpper');
     const title = document.createElement('a');
     title.innerText = track.name;
-    title.style.fontFamily = 'Acme'
+    title.style.fontFamily = 'Acme';
     title.href = track.external_urls.spotify;
     title.setAttribute('target', '_blank');
     title.classList.add('btn', 'btn-outline-success', 'my-2', 'my-sm-0');
@@ -214,6 +118,28 @@ const testWords = async (word) => {
     return tracks;
 }
 
+// console.log(testerFunction());
+
+/* Example of getting artistID without promise data
+const artistID = artists.then(artists => artists.artists.items[0].id);
+console.log(artistID);
+
+
+// create a new playlist logic button
+const newPlaylist = document.getElementById('newPlaylist');
+const createPlaylist = document.getElementById('createPlaylist');
+createPlaylist.addEventListener('click', () => {
+    if (globalUserID == '') {
+        newPlaylist.innerText = 'Must Log In First!!';
+    }
+    else if (playlistDescription == '') {
+        newPlaylist.innerText = 'Must Insert Valid Sentence!!';
+    }
+    else {
+
+    }
+})
+
 // run all functions through async await
 const testerFunction = async (artistName) => {
     let accessToken = await getToken();
@@ -223,9 +149,84 @@ const testerFunction = async (artistName) => {
     return topTracks.tracks;
 }
 
-// console.log(testerFunction());
+// create new artist card
+const makeArtistCard = (track) => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    const body = document.createElement('div');
+    body.classList.add('card-body');
+    const p = document.createElement('p');
+    p.classList.add('card-text');
+    p.innerText = track.name;
+    body.append(p);
+    card.append(body);
+    cards.append(card);
+}
 
-/* Example of getting artistID without promise data
-const artistID = artists.then(artists => artists.artists.items[0].id);
-console.log(artistID);
+// generate top ten tracks of artist
+const getTopTracks = async (artistID, token) => {
+    const result = await fetch (`https://api.spotify.com/v1/artists/${artistID}/top-tracks?country=US`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+        }
+    })
+    const data = await result.json();
+    return data;
+}
+
+// get artist ID from search result
+const getArtist = async (keyword, token) => {
+    const result = await fetch ('https://api.spotify.com/v1/search?q=' + keyword + '&type=artist', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+        }
+    })
+    const data = result.json();
+    return data;
+}
+
+
+// generate authorization code token
+const getAuthToken = async () => {
+    const result = await fetch ('https://accounts.spotify.com/authorize?client_id=' 
+    + clientId + '&scopes=playlist-read-public&response_type=code&redirect+uri=' + encodeURIComponent(redirectURI))
+    const data = result.json();
+    return data;
+}
+
+// generate new playlist using userID
+const generatePlaylist = async (userID) => {
+    let accessToken = await getToken();
+    const result = await fetch(`https://api.spotify.com/v1/users/${userID}/playlists`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + accessToken
+        },
+        body: JSON.stringify({
+            "name": "Newly Generated Playlist!",
+            "description": "testing",
+            "public": true
+        })
+    });
+    const newPlaylist = await result.json();
+    return newPlaylist;
+}
+
+// get user ID
+const getUser = async (token) => {
+    const result = await fetch ('https://api.spotify.com/v1/me', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+        }
+    })
+    const data = result.json();
+    return data;
+}
  */
